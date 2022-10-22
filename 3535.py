b11bbf5fe2889d6ad9c2def0eda536785292b1e4
andreysvc <115891868+andreysvc@users.noreply.github.com>
2022-10-22 16:38:30 +0300

    Add files via upload

---
+++ 3535.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+print(("   _~_    ")*n, end="\n")
+print(("  (o o)   ")*n, end="\n")
+print((" /  V  \  ")*n, end="\n")
+print(("/(  _  )\ ")*n, end="\n")
+print(("  ^^ ^^   ")*n, end="\n")
+print()
